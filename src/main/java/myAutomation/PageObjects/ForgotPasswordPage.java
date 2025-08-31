b63d433a7d0ff9731a1088b58c8bd8619b9691b2
aish <aish@10.0.0.62>
2025-08-31 04:15:31 -0400

    commit ForgotPasswordPage

---
+++ src/main/java/myAutomation/PageObjects/ForgotPasswordPage.java
@@ -0,0 +1,34 @@
+package myAutomation.PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class ForgotPasswordPage {
+	
+	WebDriver driver;
+	
+	By forgot_password_ele= By.linkText("Forgot Password");
+	By forgot_password_heading_text_ele= By.xpath("//div/h2");
+	By emailId_ele= By.id("email");
+	By retrieve_password_button_ele= By.xpath("//form/button/i");
+	
+	public void goToForgotPassword() {
+		
+		driver.findElement(forgot_password_ele).click();
+	}
+	
+	public void checkForgotPasswordHeading() {
+		driver.findElement(By.xpath("forgot_password_heading_text_ele")).getText();
+		Assert.assertEquals(forgot_password_heading_text_ele, "Forgot Password");
+	}
+	
+	public void enterEmail(String email) {
+		driver.findElement(emailId_ele).sendKeys(email);
+	}
+	
+	public void retrievePassword() {
+		driver.findElement(retrieve_password_button_ele).click();
+	}
+
+}
