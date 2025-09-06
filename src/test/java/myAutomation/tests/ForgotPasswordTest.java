cbd4cc431d41b668d2705c464bf00e284e811687
aish <aish@10.0.0.62>
2025-09-05 22:53:04 -0400

    commit forgot password test code

---
+++ src/test/java/myAutomation/tests/ForgotPasswordTest.java
@@ -0,0 +1,24 @@
+package myAutomation.tests;
+
+import org.testng.annotations.Test;
+
+import myAutomation.PageObjects.ForgotPasswordFinalPage;
+import myAutomation.PageObjects.ForgotPasswordPage;
+
+public class ForgotPasswordTest extends CommonTests {
+
+	@Test
+	public void testForgotPassword() {
+		
+		ForgotPasswordPage forgotpassword= new ForgotPasswordPage(driver);
+		forgotpassword.goToForgotPassword();
+		forgotpassword.checkForgotPasswordHeading();
+		forgotpassword.enterEmail("aishee@gmail.com");
+		forgotpassword.retrievePassword();
+		
+		ForgotPasswordFinalPage forgotpasswordfinal= new ForgotPasswordFinalPage(driver);
+		forgotpasswordfinal.verifyForgotPasswordFinalPageText();
+	}
+	
+	
+}
